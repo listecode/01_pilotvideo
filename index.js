350d794d115661a61d2fa4ad2b9893cd6b94d0ff
Silas List <silashlist@gmail.com>
2022-05-16 13:06:14 +0200

    listeportfolio_video

--- index.js
+++ index.js
@@ -8,9 +8,16 @@ button.addEventListener("click", darkMode);
 
 function darkMode() {
   body.classList.toggle("dark");
-  if (button.innerHTML == "Biograf Tilstand") {
-    button.innerHTML = "Lys Tilstand";
+
+  if (body.classList.contains(dark)) {
+    button.textContent = "Lys Tilstand";
   } else {
-    button.innerHTML = "Biograf Tilstand";
+    button.textContent = "Biograf Tilstand";
   }
 }
+
+// if (button.innerHTML == "Biograf Tilstand") {
+//   button.innerHTML = "Lys Tilstand";
+// } else {
+//   button.innerHTML = "Biograf Tilstand";
+// }
