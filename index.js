b4d6ecfdd1521572d8a0b761ce8680e97708a353
Silas List <silashlist@gmail.com>
2022-05-16 11:26:02 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+var body = document.querySelector("body");
+var button = document.querySelector("button");
+
+// Lav en variabel, der refererer til ".mode"
+// Lav funktionen "darkMode", som
+// a) toggle'r klassen "dark" vha. classList o
+button.addEventListener("click", darkMode);
+
+function darkMode() {
+  body.classList.toggle("dark");
+  if (button.innerHTML == "Biograf Tilstand") {
+    button.innerHTML = "Lys Tilstand";
+  } else {
+    button.innerHTML = "Biograf Tilstand";
+  }
+}
